54ac8711db6d6193a705348da23a01c12c277736
AAliAhmadi <93519287+AAliAhmadi@users.noreply.github.com>
2025-05-15 13:44:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from imdb_data import load_imdb
 from model import SentimentModel
 from database import SentimentDB
+import argparse
+from imdb_data import load_data  # or wherever your load_data function is
+
 
 def main():
-    dataset = load_imdb()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_file", type=str, default=None, help="Optional path to a custom text file")
+    args = parser.parse_args()
+
+    dataset = load_data(args.data_file)  # Use user-provided file or IMDB
+    
     model = SentimentModel()
     db = SentimentDB()
 
